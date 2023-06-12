0aafaccb0029ec9cabfaa304b896ca8be5bd9489
ArinaKaramova <karamowaarina@yandex.ru>
2023-06-12 14:16:01 +0500

    /

---
+++ IntArrayProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Task5
+{
+    public class IntArrayProcessor
+    {
+        public static int[] ProcessIntArray(int[] numbers, int p)
+        {
+            return numbers
+                .Where(x => x > 0)
+                .Distinct()
+                .OrderBy(x => x % p)
+                .ThenByDescending(x => x)
+                .ToArray();
+        }
+    }
+}
